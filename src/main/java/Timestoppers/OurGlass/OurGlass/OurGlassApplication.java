7f04958c27207be08d26985bce305b3aed808e79
anthonyrobbins <arobbins612@gmail.com>
2021-12-20 09:42:54 -0500

    OurGlass

---
+++ src/main/java/Timestoppers/OurGlass/OurGlass/OurGlassApplication.java
@@ -0,0 +1,13 @@
+package Timestoppers.OurGlass.OurGlass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurGlassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurGlassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Timestoppers/OurGlass/OurGlass/OurGlassApplicationTests.java
@@ -0,0 +1,13 @@
+package Timestoppers.OurGlass.OurGlass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurGlassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
