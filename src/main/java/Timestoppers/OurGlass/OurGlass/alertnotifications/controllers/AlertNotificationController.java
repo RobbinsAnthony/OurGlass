e43bdf5bbf25b1eb9ef6cffaf148f8211ecd1303
Jasmine Durham <jasminedurham@jasmines-air.myfiosgateway.com>
2021-12-21 13:47:39 -0500

    working on completion

---
+++ src/main/java/Timestoppers/OurGlass/OurGlass/alertnotifications/controllers/AlertNotificationController.java
@@ -0,0 +1,80 @@
+package Timestoppers.OurGlass.OurGlass.alertnotifications.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/alerts")
+public class AlertNotificationController {
+
+    private final Logger logger = LoggerFactory.getLogger(AlertNotificationController.class);
+    private AlertNotificationService alertNotificationService;
+
+    @Autowired
+    public AlertNotificationController(AlertNotificationService alertNotificationService){ this.alertNotificationService = alertNotificationService;
+
+    }
+
+    @PostMapping("")
+    public ResponseEntity<AlertNotification> createAlertNotificationRequest(@RequestBody AlertNotification, alertNotification) {
+        AlertNotification savedAlertNotification = AlertNotificationService.create(alertNotification);
+        ResponseEntity response = new ResponseEntity(savedAlertNotification, HttpStatus.Created);
+        return response;
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<AlertNotification>> getAllAlertNotification(){
+        List<AlertNotification> AlertNotification = alertNotificationService.getAllAlertNotification();
+        ResponseEntity<List<AlertNotification>> response = new ResponseEntity<>(alertNotifications, HttpStatus.OK);
+        return response;
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProfileById(@PathVariable Integer id){
+        try {
+            AlertNotification alertNotification = alertNotificationService.getAlertNotificationById(id);
+            ResponseEntity<?> response = new ResponseEntity<>(alertNotification, HttpStatus.OK);
+            return response;
+        } catch (AlertNotificationNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProfile(@PathVariable Integer employeeNumber, @RequestBody AlertNotification alertNotification){
+        try{
+            AlertNotification updatedAlertNotification = alertNotificationService.updateAlertNotification(employeeNumber, alertNotification);
+            ResponseEntity response = new ResponseEntity(updatedEmployeeNumber,HttpStatus.OK);
+            return response;
+        } catch (AlertNotificationNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+  @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProfile(@PathVariable Integer employeeNumber){
+        try{
+            alertNotificationService.deleteAlertNotification(employeeNumber);
+            return ResponseEntity
+                    .status(HttpStatus.NO_CONTENT)
+                    .build();
+        } catch (PersonNotFoundException e) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+}
+
---
+++ src/main/java/Timestoppers/OurGlass/OurGlass/alertnotifications/services/AlertNotificationServiceImpl.java
@@ -0,0 +1,69 @@
+package Timestoppers.OurGlass.OurGlass.alertnotifications.services;
+
+import Timestoppers.OurGlass.OurGlass.alertnotifications.repos.AlertNotificationRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlertNotificationImpl implements AlertNotificationService {
+
+
+    private static Logger logger = LoggerFactory.getLogger(AlertNotificationServiceImpl.class);
+
+    private AlertNotificationRepo alertNotificationRepo;
+
+    @Autowired
+    public AlertNotificationServiceImpl(AlertNotificationRepo alertNotificationRepo) {
+        this.alertNotificationRepo = alertNotificationRepo;
+    }
+
+    @Override
+    public AlertNotification create(AlertNotification alertNotification);
+
+    AlertNotification savedAlertNotification = alertNotification.save(alertNotification);
+        return savedAlertNotification;
+}
+
+    @Override
+    public AlertNotification getEmployeeNumber(Integer id) throws AlertNotificationNotFoundExpectation {
+        Optional<AlertNotification> alertOptional = alertNotificationRepo.findById(id);
+        if (alertNotification.isEmpty()) {
+            logger.error("Alert notification employee number {} does not exist");
+            throw new AlertNotificationNotFoundExpectation("Alert notificatio not found");
+        }
+        return AlertNotificationOptional.get();
+    }
+
+@Override
+public List<AlertNotification> getAllAlerts() {
+        return (List) AlertNotificationRepo.findAll();
+        }
+
+@Override
+public AlertNotification updateAlertNotification(Integer id, AlertNotification alertNotification) throws AlertNotificationNotFoundException {
+        Optional <AlertNotification> AlertNotificationOptional = AlertNotificationRepo.findById(id);
+        if(AlertNotificationOptional.isEmpty()){
+        throw new AlertNotificationNotFoundException("Alert notification does not exists, can not update");
+        }
+        AlertNotification savedAlertNotification = alertNotificationOptional.get();
+        savedAlertNotification.setEmployeeNumbers(alertNotification.getEmloyeeNumbers());
+
+        return AlertNotificationRepo.save(savedAlertNotification);
+        }
+
+@Override
+public Boolean deleteAlertNotification(Integer Id) throws AlertNotificationNotFoundException {
+        Optional <AlertNotification> alertNotificationOptional = alertNotificationRepo.findById(id);
+        if(alertNoticationOptional.isEmpty()){
+        throw new AlertNotificationNotFoundException("Alert does not exist, can not delete");
+        }
+        AlertNotification alertNotificationToDelete = alertNotificationOptional.get();
+        alertNotificationRepo.delete(alertNotificationToDelete);
+        return true;
+
+    }
+
+  }
+
